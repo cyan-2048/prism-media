a1081b2c1ccdd9135dc81feb4d7f4cb2f70ed1d3
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2024-05-09 03:06:23 +0800

    convert to esm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-module.exports = {
-  opus: require('./opus'),
-  vorbis: require('./vorbis'),
-  ...require('./core'),
+const opus = require("./opus");
+const vorbis = require("./vorbis");
+const { VolumeTransformer } = require("./core");
+
+export { opus, vorbis, VolumeTransformer };
+
+const def = {
+	opus,
+	vorbis,
+	VolumeTransformer,
 };
+
+export default def;
