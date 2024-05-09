a2081eb5ae09a3e1fe14b9dffeafdbaa36c9d991
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2024-05-09 21:33:17 +0800

    fuck

---
+++ src/index.cjs
@@ -0,0 +1,5 @@
+const opus = require("./opus");
+const vorbis = require("./vorbis");
+const { VolumeTransformer } = require("./core");
+
+module.exports = { opus, vorbis, VolumeTransformer };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-const opus = require("./opus");
-const vorbis = require("./vorbis");
-const { VolumeTransformer } = require("./core");
-
-export { opus, vorbis, VolumeTransformer };
-
-const def = {
-	opus,
-	vorbis,
-	VolumeTransformer,
-};
-
-export default def;
+export * from "./index.cjs";
+export * as default from "./index.cjs";
